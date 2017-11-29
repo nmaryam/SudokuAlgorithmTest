4c8ef94de989aa448bfd4b707900309cbc9bbe96
Maryam Naveed <maryamnaveed19@gmail.com>
2017-11-29 05:01:11 +0500

    Code Algorithm

---
+++ SudokuAlgorithmTest/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuAlgorithmTest
+{
+    class Program
+    {
+        public static int testCounter = 0;
+        static void Main(string[] args)
+        {
+            int[][] goodSudoku1 = {
+                new int[] {7,8,4,  1,5,9,  3,2,6},
+                new int[] {5,3,9,  6,7,2,  8,4,1},
+                new int[] {6,1,2,  4,3,8,  7,5,9},
+
+                new int[] {9,2,8,  7,1,5,  4,6,3},
+                new int[] {3,5,7,  8,4,6,  1,9,2},
+                new int[] {4,6,1,  9,2,3,  5,8,7},
+
+                new int[] {8,7,6,  3,9,4,  2,1,5},
+                new int[] {2,4,3,  5,6,1,  9,7,8},
+                new int[] {1,9,5,  2,8,7,  6,3,4}
+            };
+
+
+            int[][] goodSudoku2 = {
+                new int[] {1,4, 2,3},
+                new int[] {3,2, 4,1},
+
+                new int[] {4,1, 3,2},
+                new int[] {2,3, 1,4}
+            };
+
+            int[][] badSudoku1 =  {
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9},
+                new int[] {1,2,3, 4,5,6, 7,8,9}
+            };
+
+            int[][] badSudoku2 = {
+                new int[] {1,2,3,4,5},
+                new int[] {1,2,3,4},
+                new int[] {1,2,3,4},  
+                new int[] {1}
+            };
+
+            Debug.Assert(ValidateSudoku(goodSudoku1), "This is supposed to validate! It's a good sudoku!");
+            Debug.Assert(ValidateSudoku(goodSudoku2), "This is supposed to validate! It's a good sudoku!");
+            Debug.Assert(!ValidateSudoku(badSudoku1), "This isn't supposed to validate! It's a bad sudoku!");
+            Debug.Assert(!ValidateSudoku(badSudoku2), "This isn't supposed to validate! It's a bad sudoku!");
+            Console.Write("All tests are cleared :)", ++testCounter);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        static bool ValidateSudoku(int[][] puzzle)
+        {
+            int bound0 = puzzle.GetLength(0);
+            Console.Write("Running Soduko Test {0}", ++testCounter);
+            Console.WriteLine();
+            for (int i = 0; i < bound0; i++)
+            {
+                int bound1 = puzzle[i].GetLength(0);
+                if (bound0 != bound1) { return false; }
+                for (int j = 0; j < bound1; j++)
+                {
+                    int element = puzzle[i][j];
+                    for (int k = 0; k < bound1; k++)
+                    {
+                        if (k != j) if (element == puzzle[i][k]) return false;
+                        if (i != k) if (element == puzzle[k][j]) return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
